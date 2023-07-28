3a7051e87e3d910ec9a0aca53127467e75ba6d17
Sz-Tim <tim.szewczyk@gmail.com>
2023-07-28 09:33:49 +0100

    adjustment for merging dfs in 0_init or 1_current

--- code/00_fn_modelOutput.R
+++ code/00_fn_modelOutput.R
@@ -169,7 +169,7 @@ summarise_post_preds <- function(post, resp, y_i.i) {
 merge_pred_dfs <- function(files, CV=NULL) {
   f.df <- tibble(f=files, 
                  covSet=str_split(files, "/") |> 
-                   map_chr(~grep("^[0-9]", .x, value=T) |> 
+                   map_chr(~grep("^[0-9][0-9]?-", .x, value=T) |> 
                              str_split_fixed("-", 2) |> 
                              magrittr::extract(,1)))
   if(is.null(CV)) {
